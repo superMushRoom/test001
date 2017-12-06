822d229c248e44b0bf84f8380f06b822c1c99005
wenze0410 <zhangwenze0410@163.com>
2017-12-06 10:58:29 +0800

    appdel01

--- Testwz/Testwz/AppDelegate.m
+++ Testwz/Testwz/AppDelegate.m
@@ -18,6 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     UIWindow *win = [application keyWindow];
+    //
+    //
+    //
     return YES;
 }
 
